457591bb160c22b8e4da900d5f159e6bd3570682
rbnpantha <rbnpantha@gmail.com>
2017-05-14 17:17:51 -0500

    Fault Handling

--- src/com/rbn/soapdemo/webservice/ShopInfo.java
+++ src/com/rbn/soapdemo/webservice/ShopInfo.java
@@ -7,18 +7,24 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
+import com.rbn.soapdemo.exception.InvalidInputException;
+
 @WebService
-@SOAPBinding(style = Style.RPC) // default is DOCUMENT (recommended coz validation features can be added in DOCUMENT)
+@SOAPBinding(style = Style.RPC) // default is DOCUMENT (recommended coz
+								// validation features can be added in DOCUMENT)
 public class ShopInfo {
 
 	@WebMethod
 	@WebResult(partName = "lookupOutput")
-	public String getShopInfo(@WebParam(partName = "lookupInput") String property) {
+	public String getShopInfo(@WebParam(partName = "lookupInput") String property) throws InvalidInputException {
 		String response = "Invalid Property";
 		if ("shopName".equals(property)) {
 			response = "Mero Shop";
 		} else if ("since".equals(property)) {
 			response = "since 2017";
+		}
+		else{
+			throw new InvalidInputException("Invalid Input" , property + " is not a valid input");
 		}
 		return response;
 	}
